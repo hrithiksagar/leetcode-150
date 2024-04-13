59c5fa77f14d5ac9586d4a7c82ae4b6f8843f7e3
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-04-13 14:42:15 +0530

    Time: 23 ms (62.28%), Space: 12.9 MB (73.69%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        mapStoT = {} # hashmap
+        mapTtoS = {} #hashmap2
+
+        for i in range(len(s)):
+            c1 = s[i]
+            c2 = t[i]
+            if ((c1 in mapStoT and mapStoT[c1] != c2) or 
+            (c2 in mapTtoS and mapTtoS[c2] != c1)):
+                return False
+        
+            mapStoT[c1] = c2
+            mapTtoS[c2] = c1
+        return True
+         
