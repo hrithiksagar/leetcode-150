fa4689730d2d50dbe2339fab18c499fa3fa843d3
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-04-26 12:39:16 +0530

    Time: 26 ms (95.49%), Space: 11.8 MB (87.30%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -10,8 +10,64 @@ having a . in email is same as without . : Can be ignored
         having a + the next part of this can be ignored
         return number of unique emails
         can use hashsets
+        - May have one more more "." and "+"
+        - After "@" its domain name
+        - Beforee "@" its local name
+        - having "." in local name will redirect to same local name
+        - if "+" in local name, everything after that will be ignored - Not in DOMAIN NAM
+        - 
 
         """
+        a = set()
+        for eachemail in emails:
+            first, last = eachemail.split("@")
+            # print(first)
+            # print(last)
+            """
+            First = 
+            test.email+alex
+            leetcode.com
+            test.e.mail+bob.cathy
+            
+            Last = 
+            leetcode.com
+            testemail+david
+            lee.tcode.com
+            now we need to modify first and last seperately
+            
+            """
+            
+            #modifying First part
+            # if we see +, we ignore all after that
+            if "+" in first:
+                first = first[:first.index("+")]
+            
+            #replacing "." with nothing
+            first = first.replace(".", "")
+            
+            #now add first to hashset
+            a.add(first + "@" + last)
+        return len(a)
+            
+            
+            
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # unique = set()
         # for e in emails:
         #     local, domain = e.split("@") # splitting it to 2 strings
@@ -25,16 +81,16 @@ having a . in email is same as without . : Can be ignored
 
         # without using inbuilt function
 
-        unique = set()
-        for e in emails:
-            i, local = 0, ""
-            while e[i] not in ["@", "+"]:
-                if e[i] != ".":
-                    local += e[i]
-                i+=1
-            while e[i] != "@":
-                i+=1
-            domain = e[i+1:]
-            unique.add((local, domain))        
-        return len(unique)
+#         unique = set()
+#         for e in emails:
+#             i, local = 0, ""
+#             while e[i] not in ["@", "+"]:
+#                 if e[i] != ".":
+#                     local += e[i]
+#                 i+=1
+#             while e[i] != "@":
+#                 i+=1
+#             domain = e[i+1:]
+#             unique.add((local, domain))        
+#         return len(unique)
 
