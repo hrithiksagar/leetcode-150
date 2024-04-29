4507f2c8c357d9d31080b6959d9aec4d44505919
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-04-30 00:35:46 +0530

    Time: 23 ms (61.80%), Space: 12.8 MB (74.92%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -5,17 +5,61 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # Case 1 - if strings are not of eq
+        # check keys and values both. 
         mapStoT = {} # hashmap
         mapTtoS = {} #hashmap2
 
         for i in range(len(s)):
             c1 = s[i]
             c2 = t[i]
-            if ((c1 in mapStoT and mapStoT[c1] != c2) or 
-            (c2 in mapTtoS and mapTtoS[c2] != c1)):
+            if ((c1 in mapStoT and mapStoT[c1] != c2) or (c2 in mapTtoS and mapTtoS[c2] != c1)):
                 return False
         
             mapStoT[c1] = c2
             mapTtoS[c2] = c1
         return True
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         mapStoT = {} # hashmap
+#         mapTtoS = {} #hashmap2
+
+#         for i in range(len(s)):
+#             c1 = s[i]
+#             c2 = t[i]
+#             if ((c1 in mapStoT and mapStoT[c1] != c2) or 
+#             (c2 in mapTtoS and mapTtoS[c2] != c1)):
+#                 return False
+        
+#             mapStoT[c1] = c2
+#             mapTtoS[c2] = c1
+#         return True
          
