32788a2dc2e54c6c8e9b5ea105385c824572608f
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-05-23 21:23:38 +0530

    Time: 27 ms (39.12%), Space: 13 MB (62.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -13,7 +13,7 @@ class Solution(object):
         for i in range(len(s)):
             c1 = s[i]
             c2 = t[i]
-            if ((c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1)):
+            if ((c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1)): # what this line does is: FIrst checks if the element is present in the hasmap or not to avoid key error then sees that if that element is assigned to any other value in c2 or not if not then it moves. 
                 return False
         
             mapST[c1] = c2
