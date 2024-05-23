5df3711e6f64bf9c2788e771fbcf7ad1486ec215
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-05-23 22:15:24 +0530

    Time: 109 ms (92.03%), Space: 12.3 MB (37.55%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        f = [0] + flowerbed + [0] # skip first and last
+        for i in range(1, len(f)-1):
+            if f[i-1] == 0 and f[i]==0  and f[i+1] == 0: 
+                f[i] = 1
+                n -= 1
+        return n<=0
+                
+                
