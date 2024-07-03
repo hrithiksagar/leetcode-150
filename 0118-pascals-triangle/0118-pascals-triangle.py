07ee20b94564a6fd99ab21df14b5563f270d738e
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-07-04 00:35:59 +0530

    Time: 49 ms (5.19%), Space: 16.6 MB (30.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,18 +1,19 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        res = [[1]] #simple base case
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows <= 0:
+            return []
 
-        for i in range(numRows-1): #because we already made one row
-            temp = [0]+ res[-1] + [0] #just building a new temporary list instead of modifying original list
-            row = []
-            for j in range(len(res[-1])+1): #length of this row is, previous row + 1 
-                row.append(temp[j]+temp[j+1])
-            res.append(row)
+        result = [[1]]  # Start with the first row
 
-        return res
+        for i in range(1, numRows):
+            prev_row = result[-1]
+            new_row = [1]  # Start each row with 1
+            print("Loop 1 end")
+            for j in range(1, i):
+                new_row.append(prev_row[j-1] + prev_row[j])
+                print(new_row)
+            new_row.append(1)  # End each row with 1
+            print("Appended 1 in end",new_row)
+            result.append(new_row)
 
-        
+        return result
