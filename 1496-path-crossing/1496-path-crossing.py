14c5285cabd8fb2d943b015159a3c37a34ede642
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-07-08 13:16:23 +0530

    Time: 33 ms (73.35%), Space: 16.5 MB (67.39%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.py
+++ 1496-path-crossing/1496-path-crossing.py
@@ -1,19 +1,42 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        dir = {"N" : [0,1],
-               "S" : [0, -1],
-               "E" : [1,0],
-               "W" : [-1,0]
-        }
-        visit = set({0,0}) # (x,y)
-        x,y = 0, 0
-        for c in path:
-            visit.add((x,y))
-            dx, dy = dir[c]
-            x,y = x+dx, y+dy
-            if (x,y) in visit:
+        # # solution 1
+        x,y = 0,0
+        visited = set()
+        visited.add((x,y))
+        
+        for dir in path:
+            if dir == "N":
+                y+=1
+            elif dir == "S":
+                y-=1
+            elif dir == "E":
+                x+=1
+            elif dir == "W":
+                x-=1
+            if (x,y) in visited:
                 return True
+            visited.add((x,y))
         return False
+        
+        # # Solution 2
+        # dir = {"N" : [0,1],
+        #        "S" : [0, -1],
+        #        "E" : [1,0],
+        #        "W" : [-1,0]
+        # }
+        # visit = set({0,0}) # (x,y)
+        # x,y = 0, 0
+        # for c in path:
+        #     visit.add((x,y))
+        #     dx, dy = dir[c]
+        #     print("loop ",{c}, "dx = ",dx,"dy = ",dy)
+        #     print("x: ",x,"y: ",y)
+        #     x,y = x+dx, y+dy
+        #     print("x+dx: ",x,"y+dy: ",y)
+        #     if (x,y) in visit:
+        #         return True
+        # return False
         
     
         
