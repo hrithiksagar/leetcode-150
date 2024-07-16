a82b1efd9e6f290ddbc5c35d3d4bbb3df9ca71a3
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-07-16 22:37:09 +0530

    Time: 37 ms (91.08%), Space: 18.1 MB (20.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # s = s.lower()
-        # s = re.sub(r'[^a-z0-9]', '', s)
-        # print(s)
-        # if s == s[::-1]:
-        #     return True 
-        # return False
+        s = s.lower()
+        s = re.sub(r'[^a-z0-9]', '', s)
+        print(s)
+        if s == s[::-1]:
+            return True 
+        return False
         
-        l, r = 0, len(s) - 1
-
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
-        return True
+        # # Solution 2
+#         l, r = 0, len(s) - 1
+#         while l < r:
+#             while l < r and not self.alphaNum(s[l]):
+#                 l += 1
+#             while r > l and not self.alphaNum(s[r]):
+#                 r -= 1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#             l, r = l + 1, r - 1
+#         return True
     
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or 
-                ord('0') <= ord(c) <= ord('9'))
+#     def alphaNum(self, c):
+#         return (ord('A') <= ord(c) <= ord('Z') or 
+#                 ord('a') <= ord(c) <= ord('z') or 
+#                 ord('0') <= ord(c) <= ord('9'))
         
