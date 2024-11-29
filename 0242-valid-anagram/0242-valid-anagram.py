4eb4a22c4b8061ff8c1a874b3358dc86e55dd851
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-11-29 11:45:44 +0530

    Time: 19 ms (18.93%), Space: 18.1 MB (5.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,33 +1,21 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        return sorted(s) == sorted(t)
+        # else: 
+        #     set1 = set()
+        #     set2 = set()
+        #     for ele in s:
+        #         set1.add(ele)
+            
+        #     for ele in t:
+        #         set2.add(ele)
+        #     print(set1)
+        #     print("\n")
+        #     print(set2)
+        #     if set1 == set2:
+        #         return True
+        #     else:
+        #         return False
 
-        hset1 = set()
-        hset2 = set()
-
-        for i in s:
-            if i in hset1:
-                None
-            else:
-                hset1.add(i)
-                print("in S: ",hset1)
-
-        for j in t:
-            if j in hset2:
-                None
-
-            else:
-                hset2.add(j)
-                print("in T: ",hset2)
-        print(hset1)
-        print(hset2)
-
-        for char in hset1:
-            if s.count(char) != t.count(char):
-                return False
-        if hset1 == hset2:
-            return True
-        return False
-
-    
